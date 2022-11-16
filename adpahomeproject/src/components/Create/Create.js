bdbaa3bb51139554839bc457d5f9593f3dd20d2d
azeez12021 <esonboyev03@bk.ru>
2022-11-16 17:28:25 +0500

    fix

--- adpahomeproject/src/components/Create/Create.js
+++ adpahomeproject/src/components/Create/Create.js
@@ -2,127 +2,44 @@ import * as React from "react";
 import { alpha, styled } from "@mui/material/styles";
 import InputBase from "@mui/material/InputBase";
 import Box from "@mui/material/Box";
-import InputLabel from "@mui/material/InputLabel";
 import TextField from "@mui/material/TextField";
-import FormControl from "@mui/material/FormControl";
-import { Grid } from "@mui/material";
+import { Button, Grid, Typography } from "@mui/material";
+import { useTranslation } from "react-i18next";
 
-const CssTextField = styled(TextField)({
-  "& label.Mui-focused": {
-    color: "green",
-  },
-  "& .MuiInput-underline:after": {
-    borderBottomColor: "green",
-  },
-  "& .MuiOutlinedInput-root": {
-    "& fieldset": {
-      borderColor: "red",
-    },
-    "&:hover fieldset": {
-      borderColor: "yellow",
-    },
-    "&.Mui-focused fieldset": {
-      borderColor: "green",
-    },
-  },
-});
 
-const BootstrapInput = styled(InputBase)(({ theme }) => ({
-  "label + &": {
-    marginTop: theme.spacing(3),
-  },
-  "& .MuiInputBase-input": {
-    borderRadius: 4,
-    position: "relative",
-    backgroundColor: theme.palette.mode === "light" ? "#fcfcfb" : "#2b2b2b",
-    border: "1px solid #ced4da",
-    fontSize: 16,
-    width: "auto",
-    padding: "10px 12px",
-    transition: theme.transitions.create([
-      "border-color",
-      "background-color",
-      "box-shadow",
-    ]),
-    // Use the system font instead of the default Roboto font.
-    fontFamily: [
-      "-apple-system",
-      "BlinkMacSystemFont",
-      '"Segoe UI"',
-      "Roboto",
-      '"Helvetica Neue"',
-      "Arial",
-      "sans-serif",
-      '"Apple Color Emoji"',
-      '"Segoe UI Emoji"',
-      '"Segoe UI Symbol"',
-    ].join(","),
-    "&:focus": {
-      boxShadow: `${alpha(theme.palette.primary.main, 0.25)} 0 0 0 0.2rem`,
-      borderColor: theme.palette.primary.main,
-    },
-  },
-}));
-
-const RedditTextField = styled((props) => (
-  <TextField InputProps={{ disableUnderline: true }} {...props} />
-))(({ theme }) => ({
-  "& .MuiFilledInput-root": {
-    border: "1px solid #e2e2e1",
-    overflow: "hidden",
-    borderRadius: 4,
-    backgroundColor: theme.palette.mode === "light" ? "#fcfcfb" : "#2b2b2b",
-    transition: theme.transitions.create([
-      "border-color",
-      "background-color",
-      "box-shadow",
-    ]),
-    "&:hover": {
-      backgroundColor: "transparent",
-    },
-    "&.Mui-focused": {
-      backgroundColor: "transparent",
-      boxShadow: `${alpha(theme.palette.primary.main, 0.25)} 0 0 0 2px`,
-      borderColor: theme.palette.primary.main,
-    },
-  },
-}));
-
-const ValidationTextField = styled(TextField)({
-  "& input:valid + fieldset": {
-    borderColor: "green",
-    borderWidth: 2,
-  },
-  "& input:invalid + fieldset": {
-    borderColor: "red",
-    borderWidth: 2,
-  },
-  "& input:valid:focus + fieldset": {
-    borderLeftWidth: 6,
-    padding: "4px !important", // override inline-style
-  },
-});
 
 export default function CustomizedInputs() {
+  const { t } = useTranslation()
+
+  const handleSubmit = () => {
+    alert("hello world")
+  }
+
   return (
-    <Grid container direction="row" justifyContent="center">
-      <Grid xs={5}>
-        <Box
-          component="form"
-          noValidate
-          sx={{
-            display: "grid",
-            gridTemplateColumns: { sm: "1fr 1fr" },
-            gap: 2,
-            marginTop: "40px",
-          }}>
-          <ValidationTextField
-            label="Main Image"
-            required
-            variant="outlined"
-            id="validation-outlined-input"
-          />
-        </Box>
+    <Grid container
+      direction="row"
+      justifyContent="end"
+      alignItems="center">
+      <Grid item xs={6} md={8}>
+        <Typography component="h1" sx={{ paddingTop: "70px", paddingBottom: "40px", fontSize: "30px" }}>{t("admin.product_create_form.product_heading")}</Typography>
+        <form onSubmit={handleSubmit} >
+          <TextField id="outlined-basic" sx={{ width: "600px", marginBottom: "20px" }} label="Name" variant="outlined" />
+          <TextField id="outlined-basic" sx={{ width: "600px", marginBottom: "20px" }} label="Description" variant="outlined" />
+          <TextField id="outlined-basic" sx={{ width: "600px", marginBottom: "20px" }} label="Price" variant="outlined" />
+          <TextField id="outlined-basic" sx={{ width: "600px", marginBottom: "20px" }} label="Image" variant="outlined" />
+          <TextField id="outlined-basic" sx={{ width: "600px", marginBottom: "20px" }} label="Image" variant="outlined" />
+          <TextField id="outlined-basic" sx={{ width: "600px", marginBottom: "20px" }} label="Image" variant="outlined" />
+          <TextField id="outlined-basic" sx={{ width: "600px", marginBottom: "20px" }} label="Image" variant="outlined" />
+          <TextField id="outlined-basic" sx={{ width: "600px", marginBottom: "20px" }} label="Image" variant="outlined" />
+          <TextField id="outlined-basic" sx={{ width: "600px", marginBottom: "20px" }} label="Image" variant="outlined" />
+          <br></br>
+          <Button variant="contained" type="submit">Button</Button>
+        </form>
+
+
+
+
+
       </Grid>
     </Grid>
   );
