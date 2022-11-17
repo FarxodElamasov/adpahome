92a6ef4af3d0e5ffb2143c56ce37685660901cdf
Dilrux2007 <urazaliyevdilrux@gmail.com>
2022-11-17 19:43:48 +0500

    fixed

--- adpahomeproject/src/components/Product/Product.js
+++ adpahomeproject/src/components/Product/Product.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import { Grid } from '@mui/material'
+// import React from 'react'
+// import { Grid } from '@mui/material'
 
 export default function ProductList() {
+
   return (
-    <Grid container direction="row" justifyContent="center">
-      <Grid xs={5}>
-        <h1>Hello List</h1>
-      </Grid>
-    </Grid>
+    <>
+    
+    </>
   )
+  
 }
