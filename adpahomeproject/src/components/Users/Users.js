0a6f5cfd23a778bebc79c6f14cf5af9cc57b0edd
salimjon3008 <salimjontoxtaqoziyeev@gmail.com>
2022-11-28 14:55:37 +0300

    salom

--- adpahomeproject/src/components/Users/Users.js
+++ adpahomeproject/src/components/Users/Users.js
@@ -1,12 +1,13 @@
-import React from 'react'
-import { Grid } from '@mui/material'
+import React from "react";
+import { Grid } from "@mui/material";
+import MyTable from "./Table/Table";
 
 export default function Users() {
   return (
     <Grid container direction="row" justifyContent="center">
-      <Grid xs={5}>
-        <h1>Hello Users</h1>
+      <Grid xs={7}>
+        <MyTable />
       </Grid>
     </Grid>
-  )
+  );
 }
